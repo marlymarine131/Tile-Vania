bbc5ef8828fc7803993603f4387f84909ec95b51
marlymarine131 <oteeele123@gmail.com>
2023-08-29 19:33:24 +0700

    third commit

--- Assets/script/input.cs
+++ Assets/script/input.cs
@@ -1,17 +1,43 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 public class input : MonoBehaviour
 {
-    
+    [SerializeField] float speed = 10f;
+    Vector2 moveInput;
+    Rigidbody2D myRigidbody;
     void Start()
     {
-        
+        myRigidbody = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        
+        run();
+        slipChar();
+    }
+
+    private void slipChar()
+    {
+        bool turnSigh = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
+        if (turnSigh)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(myRigidbody.velocity.x), 1f);
+
+        }
+    }
+
+    void run()
+    {
+        Vector2 playerVelocity = new Vector2(moveInput.x * speed, myRigidbody.velocity.y);
+        myRigidbody.velocity = playerVelocity;
+    }
+
+    void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
     }
 }
