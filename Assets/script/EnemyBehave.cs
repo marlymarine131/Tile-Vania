3d1d3c319cc90d1d1917310a0edeee3a399d61cf
marlymarine131 <oteeele123@gmail.com>
2023-09-10 19:36:48 +0700

    1

---
+++ Assets/script/EnemyBehave.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehave : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 1f;
+    Rigidbody2D rigidBodyEnemy;
+    void Start()
+    {
+        rigidBodyEnemy = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rigidBodyEnemy.velocity = new Vector2(moveSpeed, 0f);
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        moveSpeed = -moveSpeed;
+        flipEnemy();
+
+    }
+
+    private void flipEnemy()
+    {
+        bool turnCondition = Mathf.Abs(rigidBodyEnemy.velocity.x) > Mathf.Epsilon;
+        if (turnCondition)
+        {
+            transform.localScale = new Vector2(-(Mathf.Sign(rigidBodyEnemy.velocity.x)), 1f);
+
+        }
+    }
+}
--- Assets/script/input.cs
+++ Assets/script/input.cs
@@ -9,11 +9,13 @@ public class input : MonoBehaviour
     [SerializeField] float speed = 10f;
     [SerializeField] float jumpHeight = 10f;
     [SerializeField] float climpSpeed = 5f;
+    [SerializeField] Vector2 deathAnimation  = new Vector2(10f,10f);
     Vector2 moveInput;
     Rigidbody2D myRigidbody;
     Animator myAnimator;
     CapsuleCollider2D myBodyCollider;
     BoxCollider2D myFeetCollider;
+    bool isAlive = true;
     float GravityScaleAtStart;
     void Start()
     {
@@ -26,9 +28,21 @@ public class input : MonoBehaviour
 
     void Update()
     {
+        if (!isAlive) { return; }
         run();
         slipChar();
         climpLadder();
+        die();
+    }
+
+    private void die()
+    {
+        if(myBodyCollider.IsTouchingLayers(LayerMask.GetMask("Enemy")) ||
+        myFeetCollider.IsTouchingLayers(LayerMask.GetMask("trap"))){
+            isAlive = false;
+            myAnimator.SetTrigger("Dying");
+            myRigidbody.velocity = deathAnimation;
+        }
     }
 
     private void slipChar()
@@ -43,6 +57,8 @@ public class input : MonoBehaviour
 
     void run()
     {
+        if (!isAlive) { return; }
+
         Vector2 playerVelocity = new Vector2(moveInput.x * speed, myRigidbody.velocity.y);
         myRigidbody.velocity = playerVelocity;
 
@@ -52,13 +68,17 @@ public class input : MonoBehaviour
 
     void OnMove(InputValue value)
     {
+        if (!isAlive) { return; }
+
         moveInput = value.Get<Vector2>();
     }
     void OnJump(InputValue value)
     {
+        if (!isAlive) { return; }
+
         if (!myFeetCollider.IsTouchingLayers(LayerMask.GetMask("map")))
         {
-            return; 
+            return;
         }
         if (value.isPressed)
         {
@@ -66,17 +86,21 @@ public class input : MonoBehaviour
 
         }
     }
-    void climpLadder(){
-        if(!myFeetCollider.IsTouchingLayers(LayerMask.GetMask("ladder")) ){
+    void climpLadder()
+    {
+        if (!isAlive) { return; }
+
+        if (!myFeetCollider.IsTouchingLayers(LayerMask.GetMask("ladder")))
+        {
             myRigidbody.gravityScale = GravityScaleAtStart;
             myAnimator.SetBool("isCliming", false);
-
             return;
         }
 
-        Vector2 climpVelocity = new Vector2(myRigidbody.velocity.x,moveInput.y * climpSpeed );
+        Vector2 climpVelocity = new Vector2(myRigidbody.velocity.x, moveInput.y * climpSpeed);
         myRigidbody.velocity = climpVelocity;
         myRigidbody.gravityScale = 0f;
+
         bool turnSigh = Mathf.Abs(myRigidbody.velocity.y) > Mathf.Epsilon;
         myAnimator.SetBool("isCliming", turnSigh);
     }
